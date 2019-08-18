2fc3f610f6858ed66e9291ca4431b34c71f2e4fa
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2019-08-18 14:33:01 -0300

    imprimir elementos do vetor - aula04

--- estrutura-dados-algoritmos/src/curso/loiane/estruturadados/vetor/Vetor.java
+++ estrutura-dados-algoritmos/src/curso/loiane/estruturadados/vetor/Vetor.java
@@ -1,37 +1,59 @@
 package curso.loiane.estruturadados.vetor;
 
+import java.util.Arrays;
+
 public class Vetor {
-	
+
 	private String[] elementos;
 	private int tamanho;
-	
+
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
 		this.tamanho = 0;
-		
+
 	}
-	
-	
-	// Essa forma de fazer não é eficiente quando tivermos um array de centenas posições
-	/*public void adiciona(String elemento) {
-		for(int i = 0; i < this.elementos.length ; i++) {
-			if(this.elementos[i] == null) {
-				this.elementos[i] = elemento;
-				break;
-			}
-		}
-	}*/
-	
-	/* Mais eficiente, pois não é necessário percorrer todo 
-	o array em busca de uma posição valida*/
-	public boolean adiciona(String elemento)  {
-		boolean retorno = false ;
-		if(this.tamanho < this.elementos.length) {
+
+	// Essa forma de fazer não é eficiente quando tivermos um array de centenas
+	// posições
+	/*
+	 * public void adiciona(String elemento) { for(int i = 0; i <
+	 * this.elementos.length ; i++) { if(this.elementos[i] == null) {
+	 * this.elementos[i] = elemento; break; } } }
+	 */
+
+	/*
+	 * Mais eficiente, pois não é necessário percorrer todo o array em busca de uma
+	 * posição valida
+	 */
+	public boolean adiciona(String elemento) {
+		boolean retorno = false;
+		if (this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
-			this.tamanho ++;
+			this.tamanho++;
 			retorno = true;
-		} 
+		}
 		return retorno;
+	}
+
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		for (int i = 0; i < this.tamanho - 1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+
+		if (this.tamanho > 0) {
+			s.append(this.elementos[this.tamanho-1]);
+		}
+		s.append("]");
+
+		return s.toString();
 	}
 
 }
