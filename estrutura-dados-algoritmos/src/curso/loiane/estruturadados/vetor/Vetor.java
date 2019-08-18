ed36d5d9a58c7df99de77dc2a1ed335dfd16d4ab
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2019-08-18 03:02:48 -0300

    aula03 - adiciona elemento no vetor

--- estrutura-dados-algoritmos/src/curso/loiane/estruturadados/vetor/Vetor.java
+++ estrutura-dados-algoritmos/src/curso/loiane/estruturadados/vetor/Vetor.java
@@ -3,10 +3,35 @@ package curso.loiane.estruturadados.vetor;
 public class Vetor {
 	
 	private String[] elementos;
+	private int tamanho;
 	
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+		this.tamanho = 0;
 		
+	}
+	
+	
+	// Essa forma de fazer não é eficiente quando tivermos um array de centenas posições
+	/*public void adiciona(String elemento) {
+		for(int i = 0; i < this.elementos.length ; i++) {
+			if(this.elementos[i] == null) {
+				this.elementos[i] = elemento;
+				break;
+			}
+		}
+	}*/
+	
+	/* Mais eficiente, pois não é necessário percorrer todo 
+	o array em busca de uma posição valida*/
+	public boolean adiciona(String elemento)  {
+		boolean retorno = false ;
+		if(this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho ++;
+			retorno = true;
+		} 
+		return retorno;
 	}
 
 }
---
+++ estrutura-dados-algoritmos/src/curso/loiane/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,16 @@
+package curso.loiane.estruturadados.vetor.teste;
+
+import curso.loiane.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(5);
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		
+
+	}
+
+}
