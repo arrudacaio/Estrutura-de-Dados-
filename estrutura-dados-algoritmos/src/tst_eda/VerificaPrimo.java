423d90908b372babf722e40ec13c1f8e84d314fe
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2019-08-26 01:51:18 -0300

    eh primo

--- estrutura-dados-algoritmos/src/tst_eda/VerificaPrimo.java
+++ estrutura-dados-algoritmos/src/tst_eda/VerificaPrimo.java
@@ -4,7 +4,18 @@ import java.util.*;
 
 public class VerificaPrimo {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int entrada = sc.nextInt();
+		System.out.println(ehPrimo(entrada));
+	}
+	
+	private static boolean ehPrimo(int v) {
+		for(int i = 2; i < v ; i++) {
+			if( v % i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
