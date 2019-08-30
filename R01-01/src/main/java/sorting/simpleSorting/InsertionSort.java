cc80d5e1a51a2c8d9e165a9dd09e22beca9409ce
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2019-08-29 22:53:03 -0300

    implementa mergeSort

--- R01-01/src/main/java/sorting/simpleSorting/InsertionSort.java
+++ R01-01/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -1,7 +1,6 @@
 package sorting.simpleSorting;
 
 import sorting.AbstractSorting;
-import util.Util;
 
 /**
  * As the insertion sort algorithm iterates over the array, it makes the
@@ -11,46 +10,45 @@ import util.Util;
  */
 public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
-	
-	@Override
-	public void sort(T[] array, int leftIndex, int rightIndex) {
-		boolean valid = validationInput(array, leftIndex, rightIndex);
+   @Override
+   public void sort(T[] array, int leftIndex, int rightIndex) {
+      boolean valid = validationInput(array, leftIndex, rightIndex);
 
-		if (valid) {
-			T key;
-			for (int i = leftIndex + 1; i <= rightIndex; i++) {
-				key = array[i];
-				int j = i - 1;
-				while (j >= 0 && array[j].compareTo(key) > 0) {
-					array[j + 1] = array[j];
-					j--;
-				}
-				array[j + 1] = key;
+      if (valid) {
+         T key;
+         for (int i = leftIndex + 1; i <= rightIndex; i++) {
+            key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j].compareTo(key) > 0) {
+               array[j + 1] = array[j];
+               j--;
+            }
+            array[j + 1] = key;
 
-			}
-		}
+         }
+      }
 
-	}
+   }
 
-	private boolean validationInput(T[] array, int leftIndex, int rightIndex) {
-		boolean result = true;
+   private boolean validationInput(T[] array, int leftIndex, int rightIndex) {
+      boolean result = true;
 
-		if (array == null) {
-			result = false;
-		}
-		if (array.length == 0) {
-			result = false;
-		}
-		if (leftIndex < 0 || rightIndex < 0) {
-			result = false;
-		}
-		if (leftIndex >= array.length || rightIndex >= array.length) {
-			result = false;
-		}
-		if (leftIndex > rightIndex) {
-			result = false;
-		}
-		return result;
-	}
+      if (array == null) {
+         result = false;
+      }
+      if (array.length == 0) {
+         result = false;
+      }
+      if (leftIndex < 0 || rightIndex < 0) {
+         result = false;
+      }
+      if (leftIndex >= array.length || rightIndex >= array.length) {
+         result = false;
+      }
+      if (leftIndex > rightIndex) {
+         result = false;
+      }
+      return result;
+   }
 
 }
--- R02-01/src/main/java/sorting/divideAndConquer/MergeSort.java
+++ R02-01/src/main/java/sorting/divideAndConquer/MergeSort.java
@@ -1,5 +1,7 @@
 package sorting.divideAndConquer;
 
+import java.util.Arrays;
+
 import sorting.AbstractSorting;
 
 /**
@@ -13,21 +15,52 @@ public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
 		if (validacao(array, leftIndex, rightIndex)) {
-			if(leftIndex < rightIndex) { // Condição q para de quebrar quanto tiver um array de 1 elemento
-				int meio = (leftIndex + rightIndex)/2 ; // define o meio do array
-				sort(array,leftIndex, meio); 	// divide o array da esquerda até o meio
-				sort(array, meio +1, rightIndex); // divide o array do meio +1 até o rightIndex
-				merge(array,leftIndex, meio, rightIndex); // vai mesclar os arrays 
+			if (leftIndex < rightIndex) {
+				int meio = (leftIndex + rightIndex) / 2;
+				sort(array, leftIndex, meio);
+				sort(array, meio + 1, rightIndex);
+				merge(array, leftIndex, meio, rightIndex);
 			}
 
 		}
-		throw new IllegalArgumentException("Entrada Inválida");
 
 	}
 
 	private void merge(T[] array, int leftIndex, int meio, int rightIndex) {
-		// TODO Auto-generated method stub
-		
+		T[] aux = Arrays.copyOf(array, array.length); // Preenchendo o array auxiliar com os mesmos elementos do array
+														// original
+		int i = leftIndex;
+		int k = leftIndex;
+		int middle = meio + 1;
+
+		while (i <= meio && middle <= rightIndex) { // Compara os elementos da primeira metade do array com os da
+													// segunda e preenche ordenado o array auxiliar.
+
+			if (aux[i].compareTo(aux[middle]) < 0) {
+				array[k] = aux[i];
+				i++;
+			} else {
+				array[k] = aux[middle];
+				middle++;
+			}
+			k++;
+		}
+
+		while (i <= meio) { // Preenche o array auxiliar caso tenha acabado os elementos da segunda metade
+							// do array original.
+			array[k] = aux[i];
+			i++;
+			k++;
+
+		}
+
+		while (middle <= rightIndex) { // Preenche o array auxiliar caso já tenha acabado os elementos da primeira
+										// metade do array original.
+			array[k] = aux[middle];
+			middle++;
+			k++;
+		}
+
 	}
 
 	private boolean validacao(T[] array, int leftIndex, int rightIndex) {
