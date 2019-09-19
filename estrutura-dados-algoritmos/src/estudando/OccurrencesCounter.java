63cfdd6ef8d214a65113643505bf19c6c861a160
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2019-09-19 11:08:31 -0300

    adiciona estatistica de ordem

--- estrutura-dados-algoritmos/src/estudando/OccurrencesCounter.java
+++ estrutura-dados-algoritmos/src/estudando/OccurrencesCounter.java
@@ -1,17 +1,22 @@
 package estudando;
 
 public class OccurrencesCounter {
-	
+
+	public static void main(String[] args) {
+		int[] v = { -2, 3, 4, 1, 5, 21, -1 };
+		System.out.println(orderStatic(v, 4));
+	}
+
 	private int occurencesCounter(int[] array, int x, int leftIndex, int rightIndex) {
 		int ocorrencias = 0;
-		int primeiraOcorrencia = upperBound(array,x);
-		int ultimaOcorrencia = lowerBound(array,x);
-		
-		if( ultimaOcorrencia != -1 && primeiraOcorrencia != -1) {
-			ocorrencias = (ultimaOcorrencia - primeiraOcorrencia)+1;
+		int primeiraOcorrencia = upperBound(array, x);
+		int ultimaOcorrencia = lowerBound(array, x);
+
+		if (ultimaOcorrencia != -1 && primeiraOcorrencia != -1) {
+			ocorrencias = (ultimaOcorrencia - primeiraOcorrencia) + 1;
 		}
 		return ocorrencias;
-		
+
 	}
 
 	private int lowerBound(int[] v, int x) {
@@ -54,6 +59,43 @@ public class OccurrencesCounter {
 		}
 
 		return upper;
+	}
+
+	public static int orderStatic(int[] array, int k) {
+		int maior = maior(array);
+		int menor = menor(array);
+		k--;
+		while (k > 0) {
+			int aux = maior;
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] > menor && array[i] < aux) {
+					aux = array[i];
+				}
+			}
+			menor = aux;
+			k--;
+		}
+		return menor;
+	}
+
+	private static int maior(int[] array) {
+		int maior = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > maior) {
+				maior = array[i];
+			}
+		}
+		return maior;
+	}
+
+	private static int menor(int[] array) {
+		int menor = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < menor) {
+				menor = array[i];
+			}
+		}
+		return menor;
 	}
 
 }
